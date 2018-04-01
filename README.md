c96fb15e6283bcf8f5c6ca3ec6711e95cd064e21
SeiichiN <billie175@gmail.com>
2018-04-01 13:45:13 +0900

    README.md modified.

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # UnEscMarkdown
 unEscape Markdown
+
+## 作った動機
+
+Webの中の一つの文章をMarkdownでMarkdownで記述すると表現力がアップしてわかりやすくなるが、
+Javascriptなどのスクリプトが動くのが困る。
+
+Webの中の文章は、htmlspecialchars関数でスクリプトを無効化するが、Markdownで記述した
+文章をhtmlspecialchars関数を通すと、コードを表示する部分が無効化される。
+
+たとえば、以下のような記述では、
+
+    ## Sample
+    ```
+    <?php
+      phpinfo();
+    ?>
+    ```
+このようになる。  
+![Sample1](sample1.png)
+
+だから、コードの中だけ、htmlspecialchars関数での処理を元に戻せばいい。
+
+ついでに、引用を示す部分も処理を元に戻す。
+
+## 使い方
+
+### MarkdownExtraのインストール
+
+php Markdown のページ -> [ダウンロード](https://michelf.ca/projects/php-markdown/)
+
+この中のMichelfフォルダをプロジェクトフォルダに入れる。  
+phpファイルに以下の記述を入れる。
+
+```PHP
+require_once('Michelf/MarkdownExtra.inc.php');
+use Michelf\MarkdownExtra;
+```
+### unEscMarkdownの使い方
+
+さきほどのMarkdownExtra記法の文は、
+これが $text に入っているとして、以下のようにする。
+
+```
+$newtext = unEscMark(htmlspecialchars($text, ENT_QUOTE, "UTF-8"));
+$html = MarkdownExtra::defaultTransform($newtext);
+
+<html><body>
+  <?php echo $html; ?>
+</body></html>
+```
+このようになる  
+![Sample2](sample2.png)
+
+***
+&copy; 2018 Seiichi Nukayama
---
+++ unEscMarkdown.php
@@ -0,0 +1,95 @@
+<?php
+
+define (DBNAME, 'blog.db');
+define (TABLENAME, 'blog');
+
+function h ($str) {
+	return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+}
+
+function getDB() {
+//	$db = new SQLite3(DBNAME);
+	$db = new PDO('sqlite:blog.db');
+	return $db;
+}
+
+/**
+ * unEscMark:
+ *    Markdown記法のために、htmlspecialchars関数を一部無効にする。
+ *   「```」（バッククオートが３つ連続）に囲まれた部分は、
+ *    htmlspecialchars関数を無効にする。
+ *      （例）
+ *         ```                <== 無効のはじまり
+ *         <?php
+ *             phpinfo();
+ *         ?>
+ *         ```                <== 無効のおわり
+ *    「>」は、どこであっても、htmlspecialchars関数を無効にする。
+ *
+ * Usage:
+ *   $newtext = unEscMark(htmlspecialchars($text, ENT_QUOTE, "UTF-8"))
+ *   $html = MarkdownExtra::defaultTransform($newtext);
+ *   <html><body>
+ *     <?php echo $html; ?>
+ *   </body></html>
+ * Author:
+ *     Seiichi Nukayama (c) 2018
+ */
+function unEscMark($str) {
+	$flag = 0;
+	$k = 0;
+	$s = 0;  // `のカウント
+	$newStr = '';
+	for ($i = 0; $i < strlen($str); $i++) {
+		// 1バイトをコピー
+		$newStr[$k] = $str[$i];
+		// もし、"`"（バッククオートなら）
+		if ($str[$i] === '`') {
+			$s++;
+			if ($s === 3) {         // バッククオート3文字
+				if ($flag === 0) {
+					$flag = 1;
+				} elseif ($flag === 1) {
+					$flag = 0;
+				}
+				$s = 0;             // カウントをゼロにもどす
+			}
+		}
+		// バッククオートが連続しなかったら
+		if ($s > 0 && $str[$i] !== '`') { $s = 0; }
+		
+		// echo " s= $s";
+
+		if ($flag === 1) {
+			if (substr($str, $i, 4) === '&lt;') {
+				$newStr[$k] = '<';
+				$i = $i + 3;
+			}
+			/*
+			elseif (substr($str, $i, 4) === '&gt;') {
+				$newStr[$k] = '>';
+				$i = $i + 3;
+			}
+			*/
+			elseif (substr($str, $i, 4) === '&amp;') {
+				$newStr[$k] = '&';
+				$i = $i + 4;
+			}
+			elseif (substr($str, $i, 6) === '&quot;') {
+				$newStr[$k] = '"';
+				$i = $i + 5;
+			}
+			elseif (substr($str, $i, 6) === '&#039;') {
+				$newStr[$k] = "'";
+				$i = $i + 5;
+			}
+		}
+		// > は、引用をあらわす(Markdown)
+		if (substr($str, $i, 4) === '&gt;') {
+			$newStr[$k] = '>';
+			$i = $i + 3;
+		}
+		$k++;
+	}
+	return implode($newStr);  // 配列を文字列に変換して返す
+}
